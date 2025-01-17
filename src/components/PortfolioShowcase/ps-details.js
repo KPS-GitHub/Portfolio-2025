e24676cdea93ec45678d21bc8fa68b9dcf0ea4b9
Kennan Smith <kennan.smith1@gmail.com>
2025-01-17 11:41:33 -0500

    configured portfolio details view styling for mobile breakpoints

--- src/components/PortfolioShowcase/ps-details.js
+++ src/components/PortfolioShowcase/ps-details.js
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import * as styles from "./ps-details.module.css";
 import { renderRichText } from "gatsby-source-contentful/rich-text";
 import { get } from "lodash";
@@ -10,54 +10,49 @@ const PortfolioShowcaseDetails = ({ entry, index, showDetails, setShowDetails })
   const detailsWrapRef = useRef(null);
   const detailsTitleRef = useRef(null);
   const bodyRef = useRef(null);
-  // const [bodyHeight, setBodyHeight] = useState('45vh');
 
   // Note: need to check that window is defined before using it so that the site can be rendered server-side
-  // const [windowDimensions, setWindowDimensions] = useState({
-  //   width: typeof window !== 'undefined' ? window.innerWidth : 0,
-  //   height: typeof window !== 'undefined' ? window.innerHeight : 0,
-  // });
-  // useEffect(() => {
-  //   const handleResize = () => {
-  //     // if (typeof window !== 'undefined') {
-  //     //   setWindowDimensions({
-  //     //     width: window.innerWidth,
-  //     //     height: window.innerHeight,
-  //     //   });
-  //     // };
-  //     if (detailsWrapRef.current && detailsTitleRef.current && bodyRef.current) {
-  //       const detailsWrapHeight = detailsWrapRef.current.offsetHeight;
-  //       const detailsTitleHeight = detailsTitleRef.current.offsetHeight;
-  //       const calculatedBodyHeight = detailsWrapHeight - detailsTitleHeight - 500;
-  //       setBodyHeight(`${calculatedBodyHeight}px`);
-  //     }
-  //   }
+  const [windowDimensions, setWindowDimensions] = useState({
+    width: typeof window !== 'undefined' ? window.innerWidth : 0,
+    height: typeof window !== 'undefined' ? window.innerHeight : 0,
+  });
+  useEffect(() => {
+    const handleResize = () => {
+      if (typeof window !== 'undefined') {
+        setWindowDimensions({
+          width: window.innerWidth,
+          height: window.innerHeight,
+        });
+      };
+    }
 
-  //   if (typeof window !== 'undefined') {
-  //     window.addEventListener('resize', handleResize);
-  //     return () => {
-  //       window.removeEventListener('resize', handleResize);
-  //     };
-  //   }
+    if (typeof window !== 'undefined') {
+      window.addEventListener('resize', handleResize);
+      return () => {
+        window.removeEventListener('resize', handleResize);
+      };
+    }
     
-  // }, []);
+  }, []);
 
   const titleLength = title.length;
 
-  const calculateFontSize = (length) => {
-    let baseFontSize = 2.5 - (length / 50); // base font size is 2.5rem, but decreases as title length increases
+  // base font size is 2.5rem, but decreases as title length increases and as the window size decreases
+  const calculateFontSize = (length, windowWidth, windowHeight) => {
+    let baseFontSize = 2.5 - (length / 50); 
+    if (windowWidth <= 355 || windowHeight <= 300) return `${baseFontSize - 1}rem`;
+    if (windowWidth <= 455 || windowHeight <= 400) return `${baseFontSize - .75}rem`;
+    if (windowWidth <= 991 || windowHeight <= 700) return `${baseFontSize - .5}rem`;
     return `${baseFontSize}rem`;
   };
 
-  const fontSize = calculateFontSize(titleLength);
+  const fontSize = calculateFontSize(titleLength, windowDimensions.width, windowDimensions.height);
 
   return (
     <div ref={detailsWrapRef} className={`${styles.detailsWrap} ${showDetails ? styles.detailsWrapShow : ''}`}>
       <button onClick={() => setShowDetails(false)} className={`button-clean ${styles.closeButton}`}>{`<-- Close Details`}</button>
       <h2 style={{ fontSize }} ref={detailsTitleRef} className={styles.detailsTitle}>{title}</h2>
-      <div ref={bodyRef} className={styles.body} 
-      
-      >
+      <div ref={bodyRef} className={styles.body}>
         {renderRichText(body)}
       </div>
     </div>
