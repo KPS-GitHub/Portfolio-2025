3fea8c285ddd7a22005381df3c484e1af3a1725c
Kennan Smith <kennan.smith1@gmail.com>
2025-02-13 13:32:26 -0500

    fixed build error caused by trying to access DOM window object outside of useEffect; also ensured that the screen size detection runs on page load, not just when the page is resized;

--- src/components/PortfolioShowcase/PortfolioDataVis/ps-tag-freq-chart.js
+++ src/components/PortfolioShowcase/PortfolioDataVis/ps-tag-freq-chart.js
@@ -5,13 +5,13 @@ import SectionTitle from '../../section-title';
 const TagFrequencyChart = ({ entries }) => {
   console.log('TagFrequencyChart received entries:', entries); // Debug log
 
-  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+  const [screenWidth, setScreenWidth] = useState(800);
 
   useEffect(() => {
     const handleResize = () => {
       setScreenWidth(window.innerWidth);
     };
-
+    handleResize();
     window.addEventListener('resize', handleResize);
     return () => window.removeEventListener('resize', handleResize);
   }, []);
