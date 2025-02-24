fe2a1794e59e355fac21530c6e4a69f5f26cd4a1
Kennan Smith <kennan.smith1@gmail.com>
2025-02-24 04:17:53 -0500

    removed the gatsby google gtag plugin to see if it is the plugin itself causing deploy errors rather than the config for the plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,34 +20,34 @@ module.exports = {
         host: process.env.CONTENTFUL_HOST
       },
     },
-    {
-      resolve: `gatsby-plugin-google-gtag`,
-      options: {
-        // You can add multiple tracking ids and a pageview event will be fired for all of them.
-        trackingIds: [
-          "GTM-N4FT3BD2", // Google Tag Manager / GTM
-        ],
-        // This object gets passed directly to the gtag config command
-        // This config will be shared across all trackingIds
-        gtagConfig: {
-          // optimize_id: "GTM-N4FT3BD2",
-          anonymize_ip: true,
-          cookie_expires: 0,
-        },
-        // This object is used for configuration specific to this plugin
-        pluginConfig: {
-          // Puts tracking script in the head instead of the body
-          head: false,
-          // Setting this parameter is also optional
-          respectDNT: true,
-          // Avoids sending pageview hits from custom paths
-          exclude: ["/preview/**", "/do-not-track/me/too/"],
-          // Defaults to https://www.googletagmanager.com
-          origin: "https://kennansmith.netlify.app/",
-          // Delays processing pageview events on route update (in milliseconds)
-          delayOnRouteUpdate: 0,
-        },
-      },
-    },
+    // {
+    //   resolve: `gatsby-plugin-google-gtag`,
+    //   options: {
+    //     // You can add multiple tracking ids and a pageview event will be fired for all of them.
+    //     trackingIds: [
+    //       "GTM-N4FT3BD2", // Google Tag Manager / GTM
+    //     ],
+    //     // This object gets passed directly to the gtag config command
+    //     // This config will be shared across all trackingIds
+    //     gtagConfig: {
+    //       // optimize_id: "GTM-N4FT3BD2",
+    //       anonymize_ip: true,
+    //       cookie_expires: 0,
+    //     },
+    //     // This object is used for configuration specific to this plugin
+    //     pluginConfig: {
+    //       // Puts tracking script in the head instead of the body
+    //       head: false,
+    //       // Setting this parameter is also optional
+    //       respectDNT: true,
+    //       // Avoids sending pageview hits from custom paths
+    //       exclude: ["/preview/**", "/do-not-track/me/too/"],
+    //       // Defaults to https://www.googletagmanager.com
+    //       origin: "https://kennansmith.netlify.app/",
+    //       // Delays processing pageview events on route update (in milliseconds)
+    //       delayOnRouteUpdate: 0,
+    //     },
+    //   },
+    // },
   ],
 };
